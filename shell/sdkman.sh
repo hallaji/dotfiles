77501e9ec2cb250806ff62943291c789e43defe2
Vahid Hallaji <vahid@hallaji.com>
2021-06-03 02:26:55 +1000

    feat(shell): source asdf and sdkman

--- shell/sdkman.sh
+++ shell/sdkman.sh
@@ -9,4 +9,9 @@
 # @see https://github.com/sdkman/sdkman-cli
 
 export SDKMAN_DIR="$HOME/.sdkman"
+export SDKMAN_CONFIG_DIR="$SDKMAN_DIR/etc/config"
+
+# use `-i .backup` to back up the original file
+sed -i '' 's/sdkman_auto_complete=false/sdkman_auto_complete=true/g' "$SDKMAN_CONFIG_DIR"
+
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
