5b53e042db6b4fe652f3c9f7e2dd24c9cc7d1060
Vahid Hallaji <vahid@hallaji.com>
2021-06-17 23:51:05 +1000

    refactor: source sdkman

--- shell/sdkman.sh
+++ shell/sdkman.sh
@@ -1,3 +1,4 @@
+
 # ███████╗██████╗ ██╗  ██╗███╗   ███╗ █████╗ ███╗   ██╗
 # ██╔════╝██╔══██╗██║ ██╔╝████╗ ████║██╔══██╗████╗  ██║
 # ███████╗██║  ██║█████╔╝ ██╔████╔██║███████║██╔██╗ ██║
@@ -11,7 +12,8 @@
 export SDKMAN_DIR="$HOME/.sdkman"
 export SDKMAN_CONFIG_DIR="$SDKMAN_DIR/etc/config"
 
-# use `-i .backup` to back up the original file
-sed -i '' 's/sdkman_auto_complete=false/sdkman_auto_complete=true/g' "$SDKMAN_CONFIG_DIR"
-
-[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+if [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]; then
+  # use `-i .backup` to back up the original file
+  sed -i '' 's/sdkman_auto_complete=false/sdkman_auto_complete=true/g' "$SDKMAN_CONFIG_DIR"
+  source "$SDKMAN_DIR/bin/sdkman-init.sh"
+fi
