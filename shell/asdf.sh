21d6b78077cf971cbf1b27f9ba6085ebe88bdd2d
Vahid Hallaji <vahid@hallaji.com>
2021-06-24 01:29:57 +1000

    refactor: source asdf from brew bundle

--- shell/asdf.sh
+++ shell/asdf.sh
@@ -9,5 +9,4 @@
 # @see https://asdf-vm.com
 # @see https://github.com/asdf-vm/asdf
 
-export ASDF_DIR="$HOME/.asdf"
-[[ -s "$ASDF_DIR/asdf.sh" ]] && source "$ASDF_DIR/asdf.sh"
+[[ -s "$(brew --prefix asdf)/asdf.sh" ]] && source "$(brew --prefix asdf)/asdf.sh"
