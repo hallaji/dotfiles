d0400cffef3726d0ec4a119d41d03db3497a6c79
Vahid Hallaji <vahid@hallaji.com>
2021-07-19 20:40:41 +1000

    refactor: query for port number and some more tweaks

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -13,17 +13,14 @@ alias ,c="clear"
 alias ,r="exec $SHELL -l"
 alias ,q="exit"
 alias ,s="pmset sleepnow" # mac-specific
-alias internet\?="ping 8.8.8.8"
-alias weather="curl wttr.in/Melbourne,AU"
+alias ,i="ping 8.8.8.8"
+alias ,w="curl wttr.in/Melbourne,AU"
+alias ,p='f() { lsof -nP -iTCP:"$1" -sTCP:LISTEN };f'
 
 # ┌─┐ ┬ ┬┬┌─┐┬┌─  ┌─┐┌┬┐┬┌┬┐
 # │─┼┐│ │││  ├┴┐  ├┤  │││ │
 # └─┘└└─┘┴└─┘┴ ┴  └─┘─┴┘┴ ┴
 
-# @see shell/functions.sh
-alias ,f=find_source
-alias ,d=change_source
-alias ,e=edit_source
 alias ,ed="cd $DOTFILES && $EDITOR $DOTFILES"
 
 # ┬ ┬┬┌─┐┌┬┐┌─┐┬─┐┬ ┬
@@ -37,15 +34,15 @@ alias ,hs="history | grep $* -i"
 # │ ┬│ │
 # └─┘┴ ┴
 
-alias ,gw='git config user.name && git config user.email'
-alias ,gme='git config user.email <%= ENV["USER_EMAIL"] %> && ,gw'
-alias ,gco='git config user.email <%= ENV["USER_EMAIL_CORPORATE"] %> && ,gw'
+alias ,gwho='git config user.name && git config user.email'
+alias ,gme='git config user.email <%= ENV["USER_EMAIL"] %> && ,gwho'
+alias ,gco='git config user.email <%= ENV["USER_EMAIL_CORPORATE"] %> && ,gwho'
 
 # ┌─┐┬┌─┐┬  ┌─┐┌┬┐
 # ├┤ ││ ┬│  ├┤  │
 # └  ┴└─┘┴─┘└─┘ ┴
 
-alias fl="figlet -d ~/.figlet/ $*"
-alias flh1="figlet -d ~/.figlet/ -f ANSI\ Shadow $*"
-alias flh2="figlet -d ~/.figlet/ -f Calvin\ S $*" # bold version with `-f future`
-alias flh3="figlet -d ~/.figlet/ -f wideterm $*"
+alias ,fl="figlet -d ~/.figlet/ $*"
+alias ,flh1="figlet -d ~/.figlet/ -f ANSI\ Shadow $*"
+alias ,flh2="figlet -d ~/.figlet/ -f Calvin\ S $*" # bold version with `-f future`
+alias ,flh3="figlet -d ~/.figlet/ -f wideterm $*"
