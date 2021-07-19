76dd346b109daf1f8da85162703c91441e98a6dc
Vahid Hallaji <vahid@hallaji.com>
2021-07-19 20:46:34 +1000

    refactor: command checks before path exports and eval

--- shell/jenv.sh
+++ shell/jenv.sh
@@ -9,6 +9,7 @@
 # @see http://www.jenv.be
 # @see https://github.com/jenv/jenv
 
-export PATH="$HOME/.jenv/bin:$PATH"
-
-eval "$(jenv init -)"
+if type jenv > /dev/null; then
+  export PATH="$HOME/.jenv/bin:$PATH"
+  eval "$(jenv init -)"
+fi
