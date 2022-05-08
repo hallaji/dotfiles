2df8feb760c4df04b9ff2aef4f792067b87eaace
Vahid Hallaji <vahid@hallaji.com>
2022-05-08 19:15:23 +1000

    feat: alias to change background color

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -16,6 +16,8 @@ alias ,s="pmset sleepnow" # mac-specific
 alias ,i="ping 8.8.8.8"
 alias ,w="curl wttr.in/Melbourne,AU"
 alias ,p='f() { lsof -nP -iTCP:"$1" -sTCP:LISTEN };f'
+alias ,l="curl ipinfo.io/json"
+alias ,b="wallpaper set-solid-color $*"
 
 # ┌─┐ ┬ ┬┬┌─┐┬┌─  ┌─┐┌┬┐┬┌┬┐
 # │─┼┐│ │││  ├┴┐  ├┤  │││ │
