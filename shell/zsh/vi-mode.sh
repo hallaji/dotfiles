124dd0755050620a38abedda1729be341c08dd19
Vahid Hallaji <vahid@hallaji.com>
2022-07-25 19:51:52 +1000

    refactor: tweak vi-mode bindings
    Use `Ctrl + p` to change the directory and `Ctrl + o` to open the directory in the editor.

--- shell/zsh/vi-mode.sh
+++ shell/zsh/vi-mode.sh
@@ -13,11 +13,11 @@ export KEYTIMEOUT=20
 
 # @see shell/function.sh
 zle -N edit_source_directory_widget edit_source_directory
-bindkey "^p" edit_source_directory_widget
+bindkey "^o" edit_source_directory_widget
 
 # @see shell/function.sh
 zle -N change_source_directory_widget change_source_directory
-bindkey "^o" change_source_directory_widget
+bindkey "^p" change_source_directory_widget
 
 # Navigate history
 bindkey '^k' up-history
