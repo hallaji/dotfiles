9f1da3e198e5a0107a4b744db4674519027adcf2
Vahid Hallaji <vahid@hallaji.com>
2022-09-06 23:39:23 +1000

    feat: widget function to change aws profile

--- shell/functions.sh
+++ shell/functions.sh
@@ -12,7 +12,7 @@ find_in_directory() {
   if [ ! -z "$2" ] && [ "$2" = "--edit" ]; then
     eval "$EDITOR ."
   fi
-  zle reset-prompt
+  zle && { zle reset-prompt; zle -R }
 }
 
 change_source_directory() {
@@ -27,6 +27,12 @@ change_aws_profile() {
   profile=$(grep -E '\[profile .+]' ~/.aws/config | sed -E 's/\[profile (.+)\]/\1/g' | sort | fzf)
   if [[ -n $profile ]] ; then
     export AWS_PROFILE=$profile
+    echo "Switching to $profile…"
+    aws sts get-caller-identity > /dev/null 2>&1
+    if [ "$?" != "0" ]; then
+      aws sso login
+    fi
+    zle && { zle reset-prompt; zle -R }
   else
     echo 'no AWS account provided!'
     exit 1
@@ -37,6 +43,7 @@ change_gcloud_config() {
   config=$(gcloud config configurations list | cut -d' ' -f1 | sed 1d | fzf)
   if [[ -n $config ]] ; then
     gcloud config configurations activate $config
+    zle && { zle reset-prompt; zle -R }
   else
     echo 'no Gcloud config provided!'
     exit 1
