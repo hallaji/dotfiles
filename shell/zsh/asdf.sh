780f71d2fae93f2a6c2008ebc6e45e1c8fd371ea
Vahid Hallaji <vahid@hallaji.com>
2023-07-29 11:29:11 +1000

    fix: enable asdf golang mod version

--- shell/zsh/asdf.sh
+++ shell/zsh/asdf.sh
@@ -14,3 +14,6 @@ source "$(brew --prefix asdf)/libexec/asdf.sh"
 
 # https://github.com/halcyon/asdf-java#java_home
 source ~/.asdf/plugins/java/set-java-home.zsh
+
+# https://github.com/asdf-community/asdf-golang#version-selection
+export ASDF_GOLANG_MOD_VERSION_ENABLED=true
