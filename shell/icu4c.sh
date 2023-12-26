655a8e736a9ac82e67a0f31e68c77968bbcb264f
Vahid Hallaji <vahid@hallaji.com>
2023-12-26 14:07:56 +1100

    feat: add icu4c exports

---
+++ shell/icu4c.sh
@@ -0,0 +1,13 @@
+
+# ██╗ ██████╗██╗   ██╗██╗  ██╗ ██████╗
+# ██║██╔════╝██║   ██║██║  ██║██╔════╝
+# ██║██║     ██║   ██║███████║██║
+# ██║██║     ██║   ██║╚════██║██║
+# ██║╚██████╗╚██████╔╝     ██║╚██████╗
+# ╚═╝ ╚═════╝ ╚═════╝      ╚═╝ ╚═════╝
+
+export PATH="$(brew --prefix)/opt/icu4c/bin:$PATH"
+export PATH="$(brew --prefix)/opt/icu4c/sbin:$PATH"
+export LDFLAGS="-L$(brew --prefix)/opt/icu4c/lib"
+export CPPFLAGS="-I$(brew --prefix)/opt/icu4c/include"
+export PKG_CONFIG_PATH="$(brew --prefix)/opt/icu4c/lib/pkgconfig"
