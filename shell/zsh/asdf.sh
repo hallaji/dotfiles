02a859f4c6a378426b99f2ed88f808a8addbbe02
Vahid Hallaji <vahid@hallaji.com>
2024-01-01 16:23:48 +1100

    fix: asdf java settings

--- shell/zsh/asdf.sh
+++ shell/zsh/asdf.sh
@@ -13,7 +13,7 @@
 source "$(brew --prefix asdf)/libexec/asdf.sh"
 
 # https://github.com/halcyon/asdf-java#java_home
-source ~/.asdf/plugins/java/set-java-home.zsh
+asdf where java &>/dev/null && source ~/.asdf/plugins/java/set-java-home.zsh
 
 # https://github.com/asdf-community/asdf-golang#version-selection
 export ASDF_GOLANG_MOD_VERSION_ENABLED=true
