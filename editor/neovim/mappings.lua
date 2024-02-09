61f14281a757cfaf736b0e6cff61294ac78a73c1
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2024-02-09 12:16:55 +1100

    refactor: redefine key mappings using which key

--- editor/neovim/mappings.lua
+++ editor/neovim/mappings.lua
@@ -14,127 +14,99 @@ local wk = require("which-key")
 -- │ ┬├┤ │││├┤ ├┬┘├─┤│
 -- └─┘└─┘┘└┘└─┘┴└─┴ ┴┴─┘
 
-vim.keymap.set('x', 'p', 'pgvy') -- Recopy selected area on paste operation
 vim.keymap.set('n', '<C-n>i', '<C-i>') -- Use <C-n>i to send <C-i> because <C-i> is same as <tab> when received by vim. Check Alacritty bindings for <C-i>
-vim.keymap.set('n', 'Y', 'y$') -- Yank to the end of the line
-vim.keymap.set('n', 'Q', '@q') -- Replay macro recording with Q
+vim.keymap.set('x', 'p', 'pgvy') -- Recopy selected area on paste operation
 vim.keymap.set('v', '<Leader>p', '"*p') -- Paste from system clipboard in visual/select mode
 vim.keymap.set('v', '<Leader>y', '"*y') -- Copy to system clipboard in visual/select mode
 
--- ┬  ┬┬┌─┐┬ ┬┌─┐┬
--- └┐┌┘│└─┐│ │├─┤│
---  └┘ ┴└─┘└─┘┴ ┴┴─┘
-
-vim.keymap.set('', '<Leader>/', '<Plug>NERDCommenterToggle') -- Toggle comment
-vim.keymap.set('n', '<Leader>d', ':nohlsearch<CR>') -- Remove search highlights
-vim.keymap.set('n', '<Leader>m', '<Plug>MarkdownPreviewToggle') -- Preview markdown
-
-
+-- Normal mode
 wk.register({
-  Q = { " Replay macro recording" },
-  Y = { " Yank to the end of the line" },
-  ["<C-X>"] = { " Whichkey" },
-})
+  Q = { "@q", " Replay macro recording" },
+  Y = { "y$", " Yank to the end of the line" },
+  ["<C-X>"] = { ":WhichKey<CR>", " Whichkey" },
+}, { mode = "n" })
 
+-- Normal mode with leader key
 wk.register({
-  ["/"] = { "󰆉 Comment" },
+  ["<Space>"] = { ":nohlsearch<CR>", "󰸱 Remove search highlights" },
+  ["/"] = { "<Plug>NERDCommenterToggle", "󰆉 Comment" },
+  e = { ":set relativenumber!<CR>", " Toggle relative line numbers" },
+  i = { ":IndentLinesToggle<CR>", " Toggle indent marks" },
+  l = { ":Limelight!!<CR>", "󱟇 Toggle limelight mode" },
+  m = { "<Plug>MarkdownPreviewToggle", "󰽛 Preview markdown" },
+  n = { ":set norelativenumber number!<CR>", " Toggle line numbers" },
+  r = { function() vim.wo.colorcolumn = vim.wo.colorcolumn == "" and "80,120" or "" end, " Toggle rulers" },
+  s = { ":set spell! spelllang=en_au<CR>", "󰓆 Toggle spell check (en_au)" },
+  T = { ":lua MiniTrailspace.trim()<CR>", " Trim trailing spaces" },
+  w = { ":set wrap! wrap?<CR>", "󰖶 Toggle wrap mode" },
+  z = { ":ZenMode<CR>", "󰽏 Toggle zen mode" },
+}, { mode = "n", prefix = "<Leader>" })
+
+-- Visual or select mode with leader key
+wk.register({
+  ["<Space>"] = { ":nohlsearch<CR>", "󰸱 Remove search highlights" },
+  ["/"] = { "<Plug>NERDCommenterToggle", "󰆉 Comment" },
   p = { " Paste" },
-  y = { " Yank" },
+  y = { " Copy" },
   c = {
     name = "󰅺 Comment",
   },
 }, { mode="v", prefix = "<Leader>" })
 
-wk.register({
-  ["/"] = { "󰆉 Comment" },
-  d = { "󰸱 Remove search highlights" },
-  i = { " Toggle indent marks" },
-  l = { "󱟇 Toggle limelight mode" },
-  m = { "󰽛 Preview markdown" },
-  e = { " Toggle relative numbers" },
-  n = { " Toggle numbers" },
-  r = { " Toggle rulers" },
-  s = { "󰓆 Toggle spell check (en_au)" },
-  t = { " Trim spaces" },
-  w = { "󰖶 Wrap" },
-  z = { "󰽏 Toggle zen mode" },
-}, { prefix = "<Leader>" })
-
-vim.keymap.set('n', '<Leader>t', [[:lua MiniTrailspace.trim()<CR>]], { silent = true }) -- Trim trailing whitespace
-vim.keymap.set('n', '<Leader>z', [[:ZenMode<CR>]], { silent = true }) -- Enter Zen Mode
-vim.keymap.set('n', '<C-X>', [[:WhichKey<CR>]]) -- Open WhichKey menu
-vim.keymap.set('n', '<Leader>i', [[:IndentLinesToggle<CR>]], { silent = true }) -- Toggle Indent Lines
-vim.keymap.set('n', '<Leader>l', [[:Limelight!!<CR>]], { silent = true }) -- Toggle Limelight
-vim.keymap.set('n', '<Leader>e', [[:set relativenumber!<CR>]], { silent = true }) -- Toggle relative number
-vim.keymap.set('n', '<Leader>n', [[:set norelativenumber \| set number!<CR>]], { silent = true }) -- Toggle line number and relative number
-vim.keymap.set('n', '<Leader>r', [[:execute "set colorcolumn=" . (&colorcolumn == "" ? "80,120" : "")<CR>]], { silent = true }) -- Set colorcolumn to 80 and 120, or clear it if already set
-vim.keymap.set('n', '<Leader>s', [[:set spell! spelllang=en_au<CR>]]) -- Toggle spell check with English (AU) as the language
-vim.keymap.set('n', '<Leader>w', [[:set wrap! wrap?<CR>]]) -- Toggle line wrap
-
 -- ┌┐┌┌─┐┬  ┬┬┌─┐┌─┐┌┬┐┬┌─┐┌┐┌
 -- │││├─┤└┐┌┘││ ┬├─┤ │ ││ ││││
 -- ┘└┘┴ ┴ └┘ ┴└─┘┴ ┴ ┴ ┴└─┘┘└┘
 
--- Resize splits
-vim.keymap.set('n', '<C-Left>', '<C-W><')
-vim.keymap.set('n', '<C-Down>', '<C-W>-')
-vim.keymap.set('n', '<C-Up>', '<C-W>+')
-vim.keymap.set('n', '<C-Right>', '<C-W>>')
-
--- Cycle windows (splits)
-vim.keymap.set('n', '<Tab>', '<C-W>w')
-vim.keymap.set('n', '<S-Tab>', '<C-W>W')
-
--- Cycle buffers
-vim.keymap.set('n', '[b', ':bprev<CR>')
-vim.keymap.set('n', ']b', ':bnext<CR>')
-
--- Cycle tabs
-vim.keymap.set('n', '[t', ':tabp<CR>')
-vim.keymap.set('n', ']t', ':tabn<CR>')
-
+-- Normal mode
 wk.register({
-  ["<C-H>"] = { " Go to left window" },
-  ["<C-J>"] = { " Go to down window" },
-  ["<C-K>"] = { " Go to up window" },
-  ["<C-L>"] = { " Go to right window" },
-  ["<C-\\>"] = { " Previous window" },
-  ["<C-Left>"] = { " Resize width" },
-  ["<C-Down>"] = { " Resize height" },
-  ["<C-Up>"] = { " Resize height" },
-  ["<C-Right>"] = { "  Resize width" },
-  ["<Tab>"] = { "󱞣 Circle window forward" },
-  ["<S-Tab>"] = { "󱞡 Circle window backward" },
+  -- Handled by https://github.com/christoomey/vim-tmux-navigator
+  ["<C-H>"] = { " Go to Left Window" },
+  ["<C-J>"] = { " Go to Down Window" },
+  ["<C-K>"] = { " Go to Up Window" },
+  ["<C-L>"] = { " Go to Right Window" },
+
+  ["<C-Left>"] = { "<C-W><", " Resize Width" },
+  ["<C-Down>"] = { "<C-W>-", " Resize Height" },
+  ["<C-Up>"] = { "<C-W>+", " Resize Height" },
+  ["<C-Right>"] = { "<C-W>>", "  Resize Width" },
+
+  ["<C-\\>"] = { "󰮳 Go to Recent Window" },
+  ["<Tab>"] = { "<C-W>w", "󱞣 Circle Window Forward" },
+  ["<S-Tab>"] = { "<C-W>W", "󱞡 Circle Window Backward" },
+
   ["["] = {
-    b = { " Prev buffer" },
-    t = { " Prev tab" },
+    b = { ":bprev<CR>", " Previous Buffer" },
+    t = { ":tabp<CR>", " Previous Tab" },
   },
   ["]"] = {
-    b = { " Next buffer" },
-    t = { " Next tab" },
+    b = { ":bnext<CR>", " Next Buffer" },
+    t = { ":tabn<CR>", " Next Tab" },
   },
-})
+}, { mode = "n" })
 
+-- Normal mode with leader key
 wk.register({
   c = {
     name = "󰅺 Comment",
   },
-}, { prefix = "<Leader>" })
+}, { mode = "n", prefix = "<Leader>" })
 
 -- ┌─┐─┐ ┬┌─┐┬  ┌─┐┬─┐┌─┐┬─┐
 -- ├┤ ┌┴┬┘├─┘│  │ │├┬┘├┤ ├┬┘
 -- └─┘┴ └─┴  ┴─┘└─┘┴└─└─┘┴└─
 
+-- Normal mode
 wk.register({
   ["~"] = { ":NvimTreeFocus<CR>", " Nvim Tree" },
   ["-"] = { ":NnnPicker %:p:h<CR>", " NNN Picker" },
   ["`"] = { ":NnnExplorer %:p:h<CR>", " NNN Explorer" },
-})
+}, { mode = "n" })
 
 -- ┌─┐┌─┐┌─┐┬┬  ┌─┐┌┬┐
 -- │  │ │├─┘││  │ │ │
 -- └─┘└─┘┴  ┴┴─┘└─┘ ┴
 
+-- Normal mode with leader key
 wk.register({
   p = {
     name = " Copilot",
@@ -152,12 +124,57 @@ wk.register({
     u = { ":Copilot setup<CR>", "Setup" },
     v = { ":Copilot version<CR>", "Version" },
   }
-}, { prefix = "<Leader>" })
+}, { mode = "n", prefix = "<Leader>" })
 
 -- ┌─┐┌─┐┌┬┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┌┬┐
 -- │ ┬├─┘ │   ├─┘├┬┘│ ││││├─┘ │
 -- └─┘┴   ┴   ┴  ┴└─└─┘┴ ┴┴   ┴
 
+-- Normal mode with leader key
+wk.register({
+  t = {
+    name = "󰭻 GPT",
+    c = { "<CMD>GpChatNew<CR>", "New Chat" },
+    t = { "<CMD>GpChatToggle<CR>", "Toggle Chat" },
+    f = { "<CMD>GpChatFinder<CR>", "Chat Finder" },
+
+    ["<C-x>"] = { "<CMD>GpChatNew split<CR>", "New Chat split" },
+    ["<C-v>"] = { "<CMD>GpChatNew vsplit<CR>", "New Chat vsplit" },
+    ["<C-t>"] = { "<CMD>GpChatNew tabnew<CR>", "New Chat tabnew" },
+
+    r = { "<CMD>GpRewrite<CR>", "Inline Rewrite" },
+    a = { "<CMD>GpAppend<CR>", "Append (after)" },
+    b = { "<CMD>GpPrepend<CR>", "Prepend (before)" },
+
+    g = {
+      name = "generate into new ..",
+      p = { "<CMD>GpPopup<CR>", "Popup" },
+      e = { "<CMD>GpEnew<CR>", "GpEnew" },
+      n = { "<CMD>GpNew<CR>", "GpNew" },
+      v = { "<CMD>GpVnew<CR>", "GpVnew" },
+      t = { "<CMD>GpTabnew<CR>", "GpTabnew" },
+    },
+
+    n = { "<CMD>GpNextAgent<CR>", "Next Agent" },
+    s = { "<CMD>GpStop<CR>", "GpStop" },
+    x = { "<CMD>GpContext<CR>", "Toggle GpContext" },
+
+    w = {
+      name = "Whisper",
+      w = { "<CMD>GpWhisper<CR>", "Whisper" },
+      r = { "<CMD>GpWhisperRewrite<CR>", "Whisper Inline Rewrite" },
+      a = { "<CMD>GpWhisperAppend<CR>", "Whisper Append (after)" },
+      b = { "<CMD>GpWhisperPrepend<CR>", "Whisper Prepend (before)" },
+      p = { "<CMD>GpWhisperPopup<CR>", "Whisper Popup" },
+      e = { "<CMD>GpWhisperEnew<CR>", "Whisper Enew" },
+      n = { "<CMD>GpWhisperNew<CR>", "Whisper New" },
+      v = { "<CMD>GpWhisperVnew<CR>", "Whisper Vnew" },
+      t = { "<CMD>GpWhisperTabnew<CR>", "Whisper Tabnew" },
+    },
+  },
+}, { mode = "n", prefix = "<Leader>" })
+
+-- Visual or select mode with leader key
 wk.register({
   t = {
     name = "󰭻 GPT",
@@ -199,70 +216,14 @@ wk.register({
       v = { ":<C-u>'<,'>GpWhisperVnew<CR>", "Whisper Vnew" },
       t = { ":<C-u>'<,'>GpWhisperTabnew<CR>", "Whisper Tabnew" },
     },
-    },
-}, {
-  mode = "v",
-  prefix = "<Leader>",
-  buffer = nil,
-  silent = true,
-  noremap = true,
-  nowait = true,
-})
-
-wk.register({
-    t = {
-    name = "󰭻 GPT",
-    c = { "<CMD>GpChatNew<CR>", "New Chat" },
-    t = { "<CMD>GpChatToggle<CR>", "Toggle Chat" },
-    f = { "<CMD>GpChatFinder<CR>", "Chat Finder" },
-
-    ["<C-x>"] = { "<CMD>GpChatNew split<CR>", "New Chat split" },
-    ["<C-v>"] = { "<CMD>GpChatNew vsplit<CR>", "New Chat vsplit" },
-    ["<C-t>"] = { "<CMD>GpChatNew tabnew<CR>", "New Chat tabnew" },
-
-    r = { "<CMD>GpRewrite<CR>", "Inline Rewrite" },
-    a = { "<CMD>GpAppend<CR>", "Append (after)" },
-    b = { "<CMD>GpPrepend<CR>", "Prepend (before)" },
-
-    g = {
-      name = "generate into new ..",
-      p = { "<CMD>GpPopup<CR>", "Popup" },
-      e = { "<CMD>GpEnew<CR>", "GpEnew" },
-      n = { "<CMD>GpNew<CR>", "GpNew" },
-      v = { "<CMD>GpVnew<CR>", "GpVnew" },
-      t = { "<CMD>GpTabnew<CR>", "GpTabnew" },
-    },
-
-    n = { "<CMD>GpNextAgent<CR>", "Next Agent" },
-    s = { "<CMD>GpStop<CR>", "GpStop" },
-    x = { "<CMD>GpContext<CR>", "Toggle GpContext" },
-
-    w = {
-      name = "Whisper",
-      w = { "<CMD>GpWhisper<CR>", "Whisper" },
-      r = { "<CMD>GpWhisperRewrite<CR>", "Whisper Inline Rewrite" },
-      a = { "<CMD>GpWhisperAppend<CR>", "Whisper Append (after)" },
-      b = { "<CMD>GpWhisperPrepend<CR>", "Whisper Prepend (before)" },
-      p = { "<CMD>GpWhisperPopup<CR>", "Whisper Popup" },
-      e = { "<CMD>GpWhisperEnew<CR>", "Whisper Enew" },
-      n = { "<CMD>GpWhisperNew<CR>", "Whisper New" },
-      v = { "<CMD>GpWhisperVnew<CR>", "Whisper Vnew" },
-      t = { "<CMD>GpWhisperTabnew<CR>", "Whisper Tabnew" },
-    },
   },
-}, {
-  mode = "n",
-  prefix = "<Leader>",
-  buffer = nil,
-  silent = true,
-  noremap = true,
-  nowait = true,
-})
+}, { mode = "v", prefix = "<Leader>" })
 
 -- ┌─┐┬┌┬┐
 -- │ ┬│ │
 -- └─┘┴ ┴
 
+-- Normal mode
 wk.register({
   ["["] = {
     h = { ":Gitsigns prev_hunk<CR>", " Prev Hunk (Buffer)" },
@@ -270,8 +231,9 @@ wk.register({
   ["]"] = {
     h = { ":Gitsigns next_hunk<CR>", " Next Hunk (Buffer)" },
   },
-})
+}, { mode = "n" })
 
+-- Normal mode with leader key
 wk.register({
   ["<C-G>"] = { ":Git<CR>", " Fugitive" },
   ["<C-T>"] = { ":Tig<CR>", " Tig" },
@@ -306,22 +268,30 @@ wk.register({
     u = { ":Gitsigns undo_stage_hunk<CR>", "Unstage Hunk" },
     v = { ":Gitsigns select_hunk<CR>", "Select Hunk" },
   },
-}, { prefix = "<Leader>" })
+}, { mode = "n", prefix = "<Leader>" })
 
 -- ┌┐ ┬ ┬┌─┐┌─┐┌─┐┬─┐
 -- ├┴┐│ │├┤ ├┤ ├┤ ├┬┘
 -- └─┘└─┘└  └  └─┘┴└─
 
-vim.keymap.set('n', '<C-Q>', ':q<CR>') -- Close the current buffer
-vim.keymap.set('i', '<C-Q>', '<Esc>:q<CR>') -- Exit insert mode and close the current buffer
-vim.keymap.set('x', '<C-Q>', '<Esc>') -- Exit visual mode
-vim.keymap.set('i', '<C-S>', '<C-O>:update<CR>') -- Save the current buffer
-vim.keymap.set('n', '<C-S>', ':update<CR>') -- Save the current buffer
-
+-- Normal mode
 wk.register({
-  ["<C-S>"] = { " Save" },
-  ["<C-Q>"] = { "󰗼 Quit" },
-})
+  ["<C-S>"] = { ":update<CR>", " Save" },
+  ["<C-Q>"] = { ":q<CR>", "󰗼 Quit" },
+}, { mode = "n" })
+
+-- insert mode
+wk.register({
+  ["<C-S>"] = { "<C-O>:update<CR>", " Save" },
+  ["<C-Q>"] = { "<Esc>:q<CR>", "󰗼 Quit" },
+}, { mode = "i" })
+
+-- Visual or select mode
+wk.register({
+  ["<C-Q>"] = { "<Esc>", "󰗼 Quit" },
+}, { mode = "v" })
+
+-- Normal mode with leader key
 wk.register({
   b = {
     name = " Buffer",
@@ -337,90 +307,56 @@ wk.register({
   },
   q = { ":q<CR>", "󰗼 Quit" },
   Q = { ":qa<CR>", "󰗼 Quit all" },
-}, { prefix = "<Leader>" })
+}, { mode = "n", prefix = "<Leader>" })
 
+-- ┌─┐┌─┐┌─┐   ┬   ┌─┐┌┬┐┬─┐┬  ┌─┐┌─┐
+-- ├┤ ┌─┘├┤   ┌┼─  │   │ ├┬┘│  └─┐├┤
+-- └  └─┘└    └┘   └─┘ ┴ ┴└─┴─┘└─┘└
 
-vim.keymap.set('i', '<C-f>', '<Esc>:FzfLua<CR>') -- Open FzfLua in insert mode
-vim.keymap.set('n', '<C-f>', ':FzfLua<CR>') -- Open FzfLua in normal mode
-vim.keymap.set('x', '<C-f>', '<Esc>:FzfLua<CR>') -- Open FzfLua in visual mode
-vim.keymap.set('n', '<leader>F', '<Plug>CtrlSFPrompt') -- Trigger CtrlSF prompt (provided by a plugin)
-vim.keymap.set('n', '<leader>f', ':FzfLua<CR>') -- Open FzfLua in normal mode
-vim.keymap.set('n', '<leader>fb', ':FzfLua buffers<CR>') -- Open FzfLua for buffers in normal mode
-
+-- Normal mode with leader key
 wk.register({
-  ["<C-F>"] = { " Spotlight" },
-})
-wk.register({
+  ["<C-F>"] = { ":FzfLua<CR>", "󰀶 Fuzzy Finder" },
   f = {
-    name = " Find",
+    name = " Find & Replace",
+    ["<Space>"] = { "<Plug>CtrlSFClearHL", "Clear highlights" },
+    c = { "<Plug>CtrlSFClose", "Close" },
+    f = { "<Plug>CtrlSFFocus", "Focus" },
+    l = { "<Plug>CtrlSFPwordPath", "Prompt Last Pattern" },
+    o = { "<Plug>CtrlSFOpen", "Open" },
+    p = { "<Plug>CtrlSFPrompt", "Prompt" },
+    s = { "<Plug>CtrlSFStop", "Stop Search" },
+    t = { "<Plug>CtrlSFToggle", "Toggle" },
+    u = { "<Plug>CtrlSFUpdate", "Update Results" },
+    w = { "<Plug>CtrlSFCwordPath", "Prompt Under Cursor" },
+    x = { "<Plug>CtrlSFCCwordPath", "Prompt Under Cursor with Boundary" },
+  },
+  s = {
+    name = "󰓈 Spotlight",
     b = { ":FzfLua buffers<CR>", "Buffers" },
   },
-  F = { "󰈞 Find/Replace" },
-}, { prefix = "<leader>" })
+}, { mode = "n", prefix = "<Leader>" })
 
--- ┬─┐┌─┐┌─┐┬  ┌─┐┌─┐┌─┐
--- ├┬┘├┤ ├─┘│  ├─┤│  ├┤
--- ┴└─└─┘┴  ┴─┘┴ ┴└─┘└─┘
-
-vim.keymap.set('n', '<Leader>rc', ':CtrlSFClearHL<CR>') -- Clear CtrlSF highlights
-vim.keymap.set('n', '<Leader>rf', ':CtrlSFFocus<CR>') -- Focus CtrlSF window
-vim.keymap.set('n', '<Leader>rl', '<Plug>CtrlSFPwordExec') -- Execute CtrlSF word motion
-vim.keymap.set('n', '<Leader>ro', ':CtrlSFOpen<CR>') -- Open CtrlSF
-vim.keymap.set('n', '<Leader>rp', '<Plug>CtrlSFPrompt') -- Execute CtrlSF prompt
-vim.keymap.set('n', '<Leader>rs', '<Plug>CtrlSFVwordExec') -- Execute CtrlSF visual word motion
-vim.keymap.set('n', '<Leader>rt', ':CtrlSFToggle<CR>') -- Toggle CtrlSF
-vim.keymap.set('n', '<Leader>ru', ':CtrlSFUpdate<CR>') -- Update CtrlSF index
-vim.keymap.set('n', '<Leader>rw', '<Plug>CtrlSFCwordExec') -- Execute CtrlSF word completion
-vim.keymap.set('n', '<Leader>rz', '<Plug>CtrlSFCCwordExec') -- Execute CtrlSF CamelCase word completion
-
+-- Visual or select mode with leader key
 wk.register({
-  r = {
-    name = " Find/Replace",
-    c = { "Clear Highlight" },
-    f = { "Focus Window" },
-    l = { "Last Search" },
-    o = { "Open Window" },
-    p = { "Prompt" },
-    s = { "Selected" },
-    t = { "Toggle Window" },
-    u = { "Update Last Results" },
-    w = { "Word" },
-    z = { "Word Plus Boundary" },
+  f = {
+    name = " Find & Replace",
+    e = { "<Plug>CtrlSFVwordExec", "Find Selected Area" },
+    v = { "<Plug>CtrlSFVwordPath", "Prompt Selected Area" },
   },
-}, { prefix = "<leader>" })
-
-
--- ┌─┐┌─┐┌─┐
--- ├┤ ┌─┘├┤
--- └  └─┘└
-
--- @TODO: fzf mappings
--- vim.keymap.set('n', '<leader><tab>', '<Plug>(fzf-maps-n)') -- Open Fzf-maps-n (normal mode)
--- vim.keymap.set('x', '<leader><tab>', '<Plug>(fzf-maps-x)') -- Open Fzf-maps-x (visual mode)
--- vim.keymap.set('o', '<leader><tab>', '<Plug>(fzf-maps-o)') -- Open Fzf-maps-o (operator-pending mode)
-
+}, { mode = "v", prefix = "<Leader>" })
 
 -- ┌─┐┌─┐┌─┐
 -- │  │ ││
 -- └─┘└─┘└─┘
 
--- GoTo code navigation
-vim.keymap.set('n', 'gd', '<Plug>(coc-definition)') -- Go to definition
-vim.keymap.set('n', 'gy', '<Plug>(coc-type-definition)') -- Go to type definition
-vim.keymap.set('n', 'gi', '<Plug>(coc-implementation)') -- Go to implementation
-vim.keymap.set('n', 'gr', '<Plug>(coc-references)') -- Find references
-
--- @TODO `:CocDiagnostics` to get all diagnostics of current buffer in location list
-vim.keymap.set('n', '[g', '<Plug>(coc-diagnostic-prev)') -- Go to previous diagnostic
-vim.keymap.set('n', ']g', '<Plug>(coc-diagnostic-next)') -- Go to next diagnostic
-
+-- Normal mode
 wk.register({
-  ["[g"] = { " Prev diagnostic" },
-  ["]g"] = { " Next diagnostic" },
+  ["[g"] = { "<Plug>(coc-diagnostic-prev)", " Prev Diagnostic" },
+  ["]g"] = { "<Plug>(coc-diagnostic-next)", " Next Diagnostic" },
   g = {
-    d = { " Definition" },
-    y = { " Type definition" },
-    i = { " Implementation" },
-    r = { " Reference" },
+    d = { "<Plug>(coc-definition)", "Go to Definition" },
+    y = { "<Plug>(coc-type-definition)", "Got to Type Definition" },
+    i = { "<Plug>(coc-implementation)", "Go to Implementation" },
+    r = { "<Plug>(coc-references)", "Go to Reference" },
   },
-})
+}, { mode = "n" })
