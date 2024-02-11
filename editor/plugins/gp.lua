05e387f3396b62afb1d95d81167b85348eb7f4e3
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2024-02-11 12:47:11 +1100

    feat: update user and assistance prefixes

--- editor/plugins/gp.lua
+++ editor/plugins/gp.lua
@@ -8,5 +8,12 @@
 -- https://github.com/Robitx/gp.nvim
 
 require("gp").setup({
+  -- required openai api key
   openai_api_key = { "op", "read", "--account", "my.1password.com", "<%= ENV["OPENAI_API_KEY_REFERENCE"] %>" },
+
+  -- chat user prompt prefix
+  chat_user_prefix = "󰭻",
+
+  -- chat assistant prompt prefix
+  chat_assistant_prefix = { "󰵰  ", "[{{agent}}]" },
 })
