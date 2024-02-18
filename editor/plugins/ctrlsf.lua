f4890dc82d980a83a152750b51b15ad28cb15465
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2024-02-18 16:13:04 +1100

    feat: ctrlsf settings

--- editor/plugins/ctrlsf.lua
+++ editor/plugins/ctrlsf.lua
@@ -8,6 +8,10 @@
 -- https://github.com/dyng/ctrlsf.vim
 -- https://github.com/dyng/ctrlsf.vim/blob/master/doc/ctrlsf.txt
 
-vim.g.ctrlsf_default_view_mode = 'normal' -- Default view mode (normal, compact, minimal)
+vim.g.ctrlsf_default_view_mode = 'normal' -- Default view mode (normal, compact)
 vim.g.ctrlsf_position = 'left' -- Default position (left, right, top, bottom)
 vim.g.ctrlsf_compact_position = 'bottom_outside' -- Default compact position (left_outside, right_outside, top_outside, bottom_outside)
+vim.g.ctrlsf_default_root = 'project' -- Default root (project, cwd)
+vim.g.ctrlsf_preview_position = 'inside' -- Default preview position (inside, outside)
+vim.g.ctrlsf_auto_preview = 1 -- Automatically preview the search result
+vim.g.ctrlsf_auto_close = { normal = 1, compact = 0 } -- Automatically close the window after the search
