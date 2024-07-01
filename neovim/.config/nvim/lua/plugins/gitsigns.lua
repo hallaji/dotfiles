5ec606b4225bdd1ffaf755d5cb5fa35da862cf42
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2024-07-01 12:38:59 +1000

    fix: use default signs

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -11,14 +11,7 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require('gitsigns').setup {
-      signs = {
-        add          = { text = '' },
-        change       = { text = '' },
-        delete       = { text = '﹏' },
-        topdelete    = { text = '﹋' },
-        changedelete = { text = '〰' },
-        untracked    = { text = '┆' },
-      },
+      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_formatter = '    (<author>, <author_time:%R>) <summary>',
     }
