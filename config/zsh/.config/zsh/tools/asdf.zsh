870c912fced4f7f3be01cb47546d30243694de76
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2024-11-09 01:15:25 +1100

    fix: sourcing asdf and adding additional installations

--- config/zsh/.config/zsh/tools/asdf.zsh
+++ config/zsh/.config/zsh/tools/asdf.zsh
@@ -7,10 +7,15 @@
 # https://asdf-vm.com
 # https://github.com/asdf-vm/asdf
 
-# ZSH & Homebrew
-if type asdf &> /dev/null; then
-  source "$(brew --prefix asdf)/libexec/asdf.sh"
 
+if [[ "$OSTYPE" == "darwin"* ]] && type brew &> /dev/null; then
+  source "$(brew --prefix asdf)/libexec/asdf.sh" 2>/dev/null || :
+else
+  source /opt/asdf-vm/asdf.sh 2>/dev/null || :
+fi
+
+
+if type asdf &> /dev/null; then
   # https://github.com/halcyon/asdf-java#java_home
   asdf where java &>/dev/null && source ~/.asdf/plugins/java/set-java-home.zsh
 
