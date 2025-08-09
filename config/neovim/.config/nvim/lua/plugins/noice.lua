78d2e00826ca46f582b9e095ec24730a08710a0e
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2025-08-09 18:50:09 +1000

    refactor: replace fidget with noice

---
+++ config/neovim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,19 @@
+-- ███╗   ██╗ ██████╗ ██╗ ██████╗███████╗
+-- ████╗  ██║██╔═══██╗██║██╔════╝██╔════╝
+-- ██╔██╗ ██║██║   ██║██║██║     █████╗
+-- ██║╚██╗██║██║   ██║██║██║     ██╔══╝
+-- ██║ ╚████║╚██████╔╝██║╚██████╗███████╗
+-- ╚═╝  ╚═══╝ ╚═════╝ ╚═╝ ╚═════╝╚══════╝
+-- https://github.com/folke/noice.nvim
+
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+  "MunifTanjim/nui.nvim",
+  "rcarriga/nvim-notify",
+  }
+}
