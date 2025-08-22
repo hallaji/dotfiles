ecb955d5896a9280dbb93b3ceaef8075196a99d2
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2025-08-23 00:38:31 +1000

    fix: tweak sketchybar config

--- config/sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ config/sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -2,4 +2,17 @@
 
 FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
-sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE"
+case $FOCUSED_WORKSPACE in
+    1) SYMBOL="󰬺" ;;
+    2) SYMBOL="󰬻" ;;
+    3) SYMBOL="󰬼" ;;
+    4) SYMBOL="󰬽" ;;
+    5) SYMBOL="󰬾" ;;
+    6) SYMBOL="󰬿" ;;
+    7) SYMBOL="󰭀" ;;
+    8) SYMBOL="󰭁" ;;
+    9) SYMBOL="󰭂" ;;
+    *) SYMBOL="$FOCUSED_WORKSPACE" ;;
+esac
+
+sketchybar --set "$NAME" label="$SYMBOL"
