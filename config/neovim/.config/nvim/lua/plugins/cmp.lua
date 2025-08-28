64d604fc35ffb41098314c360599a9da7531ae3c
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2025-08-28 23:07:18 +1000

    fix: support completion in gitcommit buffer

--- config/neovim/.config/nvim/lua/plugins/cmp.lua
+++ config/neovim/.config/nvim/lua/plugins/cmp.lua
@@ -6,6 +6,8 @@
 --  ╚═════╝╚═╝     ╚═╝╚═╝
 -- https://github.com/hrsh7th/nvim-cmp
 -- https://github.com/hrsh7th/cmp-nvim-lsp
+-- https://github.com/hrsh7th/cmp-buffer
+-- https://github.com/hrsh7th/cmp-path
 -- https://github.com/L3MON4D3/LuaSnip
 -- https://github.com/saadparwaiz1/cmp_luasnip
 
@@ -14,6 +16,8 @@ return {
   cond = not vim.g.vscode,
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",     -- LSP completions
+    "hrsh7th/cmp-buffer",       -- Buffer completions
+    "hrsh7th/cmp-path",         -- Path completions
     "L3MON4D3/LuaSnip",         -- Snippet engine
     "saadparwaiz1/cmp_luasnip", -- Registers LuaSnip as a completion source
   },
@@ -40,18 +44,20 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
 
-      sources = cmp.config.sources(
-        {
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
-          -- { name = "vsnip" }, -- For vsnip users.
-          -- { name = "snippy" }, -- For snippy users.
-          -- { name = "ultisnips" }, -- For ultisnips users.
-        },
-        {
-          { name = "buffer" },
-        }
-      ),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+
+    -- Enable completion in git commit buffers
+    cmp.setup.filetype('gitcommit', {
+      sources = cmp.config.sources({
+        { name = "buffer" },
+        { name = "path" },
+      })
     })
   end,
 }
