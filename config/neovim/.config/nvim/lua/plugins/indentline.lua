975771bc5cc041435195db6e1c461afc868a3f6e
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2025-08-29 17:25:30 +1000

    refactor: add file and buffer type exclusions

--- config/neovim/.config/nvim/lua/plugins/indentline.lua
+++ config/neovim/.config/nvim/lua/plugins/indentline.lua
@@ -14,15 +14,16 @@
 
 return {
   "Yggdroot/indentLine",
+  enabled = false,
   cond = not vim.g.vscode,
   config = function()
     vim.g.indentLine_enabled = 1 -- Enable indentLine
     vim.g.indentLine_setColors = 0 -- Disable default colors
-    vim.g.indentLine_char = '' -- Use a thin vertical bar as the indent character
-    vim.g.indentLine_fileTypeExclude = {'text', 'startify', 'noice', 'mason'} -- Disable indentLine for text and startify buffers
-    vim.g.indentLine_bufTypeExclude = {'help', 'terminal'} -- Disable indentLine for help and terminal buffers
+    vim.g.indentLine_char = "" -- Use a thin vertical bar as the indent character
+    vim.g.indentLine_fileTypeExclude = { "text", "startify", "noice", "mason", "snacks_terminal", "snacks_dashboard" } -- Disable indentLine for text and startify buffers
+    vim.g.indentLine_bufTypeExclude = { "help", "terminal", "nofile", "nowrite", "prompt", "quickfix" } -- Disable indentLine for help and terminal buffers
     vim.g.indentLine_leadingSpaceEnabled = 1 -- Enable leading space
-    vim.g.indentLine_leadingSpaceChar = '┈' -- Use a thin vertical bar as the leading space character
+    vim.g.indentLine_leadingSpaceChar = "┈" -- Use a thin vertical bar as the leading space character
     vim.g.vim_json_conceal = 0 -- Disable JSON syntax conceal
     vim.g.markdown_syntax_conceal = 0 -- Disable Markdown syntax conceal
   end,
