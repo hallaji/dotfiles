a676f773fc129b4e54568195a7513e653fe9fac9
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2025-09-14 09:27:54 +1000

    fix: hostname display

--- config/sketchybar/.config/sketchybar/plugins/host.sh
+++ config/sketchybar/.config/sketchybar/plugins/host.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 HOSTNAME=$(hostname | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
-case "$(hostname)" in
+case "$(hostname | tr '[:upper:]' '[:lower:]')" in
   vhallaji-41NW96)
     export HOSTNAME="Monomaran"
+    ;;
+  catamaran.local)
+    export HOSTNAME="Catamaran"
     ;;
 esac
 
