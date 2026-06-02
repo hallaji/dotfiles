5397ac50598d4563a9f843f86ff19fbe95a7916c
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2026-06-02 23:33:42 +1000

    feat: navigation layer mods

--- config/keyboards/zsa/voyager/keymaps/hallaji/keymap.c
+++ config/keyboards/zsa/voyager/keymaps/hallaji/keymap.c
@@ -24,7 +24,7 @@ enum tap_dance_codes {
   DANCE_3,
 };
 
-#define DUAL_FUNC_0 LT(13, KC_7)
+#define DUAL_FUNC_0 LT(13, KC_6)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_voyager(
     KC_NO,          KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         QK_LLCK,        
     KC_NO,          KC_F11,         KC_F12,         KC_F13,         KC_F14,         KC_F15,                                         KC_F16,         KC_F17,         KC_F18,         KC_F19,         KC_F20,         KC_NO,          
-    KC_TRANSPARENT, KC_NO,          KC_HOME,        KC_PAGE_UP,     KC_PGDN,        KC_END,                                         KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,          KC_NO,          
+    KC_TRANSPARENT, KC_LEFT_GUI,    MT(MOD_LALT, KC_HOME),MT(MOD_LSFT, KC_PAGE_UP),MT(MOD_LCTL, KC_PGDN),ALL_T(KC_END),                                  ALL_T(KC_LEFT), MT(MOD_RCTL, KC_DOWN),MT(MOD_RSFT, KC_UP),MT(MOD_RALT, KC_RIGHT),KC_RIGHT_GUI,   KC_NO,          
     KC_NO,          KC_F21,         KC_F22,         KC_F23,         KC_F24,         KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
