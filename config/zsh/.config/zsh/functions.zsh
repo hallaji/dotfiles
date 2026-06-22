5cafb153b4c1cbfbac415bbc9f8c0b2ce64dda65
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2026-06-22 23:46:31 +1000

    refactor: update dotup completion

--- config/zsh/.config/zsh/functions.zsh
+++ config/zsh/.config/zsh/functions.zsh
@@ -171,7 +171,7 @@ _dotup_completion() {
     'asdf:ASDF plugins'
     'cli:CLI tools'
     'mac:MacOS-specific installations'
-    'personal:Manage personal local config'
+    'personal:Manage personal settings'
     'services:System services'
     'shell:Shell installations'
     'vim:Vim/Neovim installations'
