e98fcaddabd2bbe182991c6c810f6e514474f44e
Vahid Hallaji <1447542+hallaji@users.noreply.github.com>
2026-07-07 20:41:57 +1000

    chore: remove pyenv

--- config/zsh/.config/zsh/tools.zsh
+++ config/zsh/.config/zsh/tools.zsh
@@ -158,21 +158,6 @@ case ":$PATH:" in
 esac
 
 
-# ┌─┐┬ ┬┌─┐┌┐┌┬  ┬
-# ├─┘└┬┘├┤ │││└┐┌┘
-# ┴   ┴ └─┘┘└┘ └┘
-# https://github.com/pyenv/pyenv
-
-if [ -d "$HOME/.pyenv" ]; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-fi
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init --path)"
-  eval "$(pyenv virtualenv-init -)"
-fi
-
-
 # ┬─┐┌─┐┌┐┌┌─┐┬ ┬┌─┐┬─┐
 # ├┬┘├─┤││││  ├─┤├┤ ├┬┘
 # ┴└─┴ ┴┘└┘└─┘┴ ┴└─┘┴└─
